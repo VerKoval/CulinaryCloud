6c47c128f15aa41b17f6d52c6d41e0417dc72b66
Bismahhasan <hasanbismah@gmail.com>
2024-05-10 11:27:02 -0400

    Track style.css in the static directory

--- templates/view_menu.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-from app import app  # Import the Flask app instance
-
-@app.route('/view_menu')  # Adjust the route to remove extra spaces
-def view_menu():
-    return render_template('view_menu.html')  # Assuming you have a view_menu.html template
-
