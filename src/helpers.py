b4386dc02e2f854ff3a285271ce04e5adba794e0
Bismahhasan <hasanbismah@gmail.com>
2024-05-11 18:33:43 -0400

    Restored src directory and updated project

---
+++ src/helpers.py
@@ -0,0 +1,19 @@
+
+# utils.py
+import random
+import re
+
+def validate_employee_id(role, employee_id):
+    prefix = {
+        'chef': '2',
+        'delivery': '3',
+        'food_importer': '4',
+        'manager': '5'
+    }
+    if role in prefix:
+        if str(employee_id).startswith(prefix[role]):
+            return True
+        else:
+            return False
+    else:
+        return False
