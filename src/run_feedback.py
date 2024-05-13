ed85d7486f4b5c99c60da0941bbde117ef44406a
Akshay Raju <akshayraju@Akshays-MacBook-Air.local>
2024-05-13 18:24:39 -0400

    Automatic ID generation for Chef

--- src/run_feedback.py
+++ src/run_feedback.py
@@ -1,8 +1,8 @@
 from Chef import Chef
 import Database
 
-chef1 = Chef(1001, 'John Smith', 50000)
-chef2 = Chef(1002, 'Rosalyn Franklin', 50000)
+chef1 = Chef('John Smith', 50000)
+chef2 = Chef('Rosalyn Franklin', 50000)
 
 chef1.raiseQualityIssue(2001,'Carrots were not of good quality')
 chef1.raiseQualityIssue(2001,'Beans were not of good quality')
